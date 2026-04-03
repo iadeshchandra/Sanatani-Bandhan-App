0122d5f6928a20dc348428f0440d7f855b2b86ac
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-04 00:31:13 +0600

    Update MemberActivity.java

--- app/src/main/java/org/shda/MemberActivity.java
+++ app/src/main/java/org/shda/MemberActivity.java
@@ -8,8 +8,7 @@ public class MemberActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Member Directory Loaded", Toast.LENGTH_SHORT).show();
-        // Uses the same logic style as TransactionActivity to save names/phones to Firebase
+        Toast.makeText(this, "Members feature coming soon", Toast.LENGTH_SHORT).show();
         finish(); 
     }
 }
