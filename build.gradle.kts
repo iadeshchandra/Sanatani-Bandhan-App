88b751a281ba023667cca99c86cdaca741a62074
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-03 17:16:05 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    
+    // Updated from 4.4.1 to 4.4.2 to fix the GitHub Actions crash
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
