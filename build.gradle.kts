09d79ba8b217ed7df5d2fd233fdb07fa5ca011f8
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-04 17:24:31 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,18 +5,14 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        // Android Gradle Plugin
-        classpath "com.android.tools.build:gradle:8.1.0"
-        
-        // Google Services plugin (Required for Firebase)
-        classpath "com.google.gms:google-services:4.4.1"
-        
-        // NEW: Crashlytics Plugin for App Monitoring
-        classpath "com.google.firebase:firebase-crashlytics-gradle:2.9.9"
+        // Notice the parentheses and double quotes required by Kotlin DSL
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.google.gms:google-services:4.4.1")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
     }
 }
 
 plugins {
-    id 'com.android.application' version '8.1.0' apply false
-    id 'com.android.library' version '8.1.0' apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
 }
