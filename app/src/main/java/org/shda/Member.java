e4f0c280e743ad0a06de7b14c265f5c93c2df7f3
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-05 00:52:38 +0600

    Update Member.java

--- app/src/main/java/org/shda/Member.java
+++ app/src/main/java/org/shda/Member.java
@@ -1,29 +1,29 @@
 package org.shda;
 
 public class Member {
-    public String id;
-    public String name;
-    public String phone;
-    public String gotra;
-    public String bloodGroup;
+    public String id, name, phone, gotra, bloodGroup, role, password;
+    // NEW OPTIONAL FIELDS
+    public String fatherName, motherName, nid, address, addedBySignature; 
     public long timestamp;
     public float totalDonated;
-    
-    // Security & Roles
-    public String role; 
-    public String password; 
 
-    public Member() {}
+    public Member() {} // Required for Firebase
 
-    public Member(String id, String name, String phone, String gotra, String bloodGroup, long timestamp, String role, String password) {
+    public Member(String id, String name, String phone, String gotra, String bloodGroup, long timestamp, String role, String password, String fatherName, String motherName, String nid, String address, String addedBySignature) {
         this.id = id;
         this.name = name;
         this.phone = phone;
         this.gotra = gotra;
         this.bloodGroup = bloodGroup;
         this.timestamp = timestamp;
-        this.totalDonated = 0.0f;
         this.role = role;
         this.password = password;
+        this.totalDonated = 0f;
+        
+        this.fatherName = fatherName;
+        this.motherName = motherName;
+        this.nid = nid;
+        this.address = address;
+        this.addedBySignature = addedBySignature;
     }
 }
