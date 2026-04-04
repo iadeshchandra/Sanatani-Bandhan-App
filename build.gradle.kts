399f0fada424feaa46adfa0653492e203fb2948f
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-04 15:55:47 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,22 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Android Gradle Plugin
+        classpath "com.android.tools.build:gradle:8.1.0"
+        
+        // Google Services plugin (Required for Firebase)
+        classpath "com.google.gms:google-services:4.4.1"
+        
+        // NEW: Crashlytics Plugin for App Monitoring
+        classpath "com.google.firebase:firebase-crashlytics-gradle:2.9.9"
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id 'com.android.application' version '8.1.0' apply false
+    id 'com.android.library' version '8.1.0' apply false
 }
