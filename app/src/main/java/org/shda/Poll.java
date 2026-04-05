00077f0711ec72ae94324c868d1bde064dfdddb3
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-05 10:52:52 +0600

    Update Poll.java

--- app/src/main/java/org/shda/Poll.java
+++ app/src/main/java/org/shda/Poll.java
@@ -3,19 +3,21 @@ package org.shda;
 import java.util.HashMap;
 
 public class Poll {
-    public String id, question, optionA, optionB, createdBy, officialComment;
-    public long timestamp;
+    public String id, question, optionA, optionB, optionC, optionD, createdBy, officialComment;
+    public long timestamp, endTimestamp;
     public HashMap<String, String> votes;
 
-    // 🔥 CRITICAL FIX: Firebase requires this empty constructor, or it crashes!
-    public Poll() {}
+    public Poll() {} // Required for Firebase
 
-    public Poll(String id, String question, String optionA, String optionB, long timestamp, String createdBy) {
+    public Poll(String id, String question, String optionA, String optionB, String optionC, String optionD, long timestamp, long endTimestamp, String createdBy) {
         this.id = id;
         this.question = question;
         this.optionA = optionA;
         this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
         this.timestamp = timestamp;
+        this.endTimestamp = endTimestamp;
         this.createdBy = createdBy;
         this.votes = new HashMap<>();
     }
