d63e97aee9499e694db042d1a5df53521e884692
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-06 14:05:02 +0600

    Update PanchangEngine.java

--- app/src/main/java/org/shda/PanchangEngine.java
+++ app/src/main/java/org/shda/PanchangEngine.java
@@ -7,37 +7,53 @@ import java.util.Locale;
 
 public class PanchangEngine {
     
-    // Hardcoded major 2026 Tithis for high accuracy offline
+    // Official 2026 ISKCON (Rangpur, BD) Ekadashi List
     private static final HashMap<String, String> tithiMap = new HashMap<>();
     
     static {
-        // April 2026
-        tithiMap.put("07 Apr 2026", "Papmochani Ekadashi");
-        tithiMap.put("12 Apr 2026", "Amavasya (Somvati)");
-        tithiMap.put("22 Apr 2026", "Kamada Ekadashi");
-        tithiMap.put("27 Apr 2026", "Chaitra Purnima / Hanuman Jayanti");
-        // May 2026
-        tithiMap.put("07 May 2026", "Varuthini Ekadashi");
-        tithiMap.put("12 May 2026", "Vaishakha Amavasya");
-        tithiMap.put("22 May 2026", "Mohini Ekadashi");
-        tithiMap.put("26 May 2026", "Buddha Purnima");
-        // June 2026
-        tithiMap.put("06 Jun 2026", "Apara Ekadashi");
-        tithiMap.put("20 Jun 2026", "Nirjala Ekadashi");
-        // July 2026
-        tithiMap.put("05 Jul 2026", "Yogini Ekadashi");
-        tithiMap.put("20 Jul 2026", "Devshayani Ekadashi");
-        // August 2026
-        tithiMap.put("04 Aug 2026", "Kamika Ekadashi");
-        tithiMap.put("19 Aug 2026", "Shravana Putrada Ekadashi");
-        // Add more as needed!
+        // January
+        tithiMap.put("14 Jan 2026", "Shattila Ekadashi (ষটতিলা একাদশী)");
+        tithiMap.put("29 Jan 2026", "Bhaimi Ekadashi (ভৈমী একাদশী)");
+        // February
+        tithiMap.put("13 Feb 2026", "Vijaya Ekadashi (বিজয়া একাদশী)");
+        tithiMap.put("27 Feb 2026", "Amalaki Vrata Ekadashi (আমলকীব্রত একাদশী)");
+        // March
+        tithiMap.put("15 Mar 2026", "Papmochani Ekadashi (পাপমোচনী একাদশী)");
+        tithiMap.put("29 Mar 2026", "Kamada Ekadashi (কামদা একাদশী)");
+        // April
+        tithiMap.put("13 Apr 2026", "Varuthini Ekadashi (বরুথিনী একাদশী)");
+        tithiMap.put("27 Apr 2026", "Mohini Ekadashi (মোহিনী একাদশী)");
+        // May
+        tithiMap.put("13 May 2026", "Apara Ekadashi (অপরা একাদশী)");
+        tithiMap.put("27 May 2026", "Padmini Ekadashi (পদ্মিনী একাদশী)");
+        // June
+        tithiMap.put("11 Jun 2026", "Parama Ekadashi (পরমা একাদশী)");
+        tithiMap.put("26 Jun 2026", "Pandava Nirjala Ekadashi (পাণ্ডবনির্জালা একাদশী)");
+        // July
+        tithiMap.put("11 Jul 2026", "Yogini Ekadashi (যোগিনী একাদশী)");
+        tithiMap.put("25 Jul 2026", "Shayan Ekadashi (শয়ন একাদশী)");
+        // August
+        tithiMap.put("09 Aug 2026", "Kamika Ekadashi (কামিকা একাদশী)");
+        tithiMap.put("24 Aug 2026", "Pavitraropana Ekadashi (পবিত্রারোপণ একাদশী)");
+        // September
+        tithiMap.put("07 Sep 2026", "Annada Ekadashi (অন্নদা একাদশী)");
+        tithiMap.put("22 Sep 2026", "Parshva Ekadashi (পার্শ্ব একাদশী)");
+        // October
+        tithiMap.put("06 Oct 2026", "Indira Ekadashi (ইন্দিরা একাদশী)");
+        tithiMap.put("22 Oct 2026", "Pashankusha Ekadashi (পাশাঙ্কুশা একাদশী)");
+        // November
+        tithiMap.put("05 Nov 2026", "Rama Ekadashi (রমা একাদশী)");
+        tithiMap.put("21 Nov 2026", "Utthana Ekadashi (উত্থান একাদশী)");
+        // December
+        tithiMap.put("05 Dec 2026", "Utpanna Ekadashi (উত্পন্না একাদশী)");
+        tithiMap.put("20 Dec 2026", "Mokshada Ekadashi (মোক্ষদা একাদশী)");
     }
 
     public static String getTodayTithiAlert() {
         SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH);
         String today = sdf.format(new Date());
         
-        // Check for Tomorrow
+        // Check for Tomorrow (Adds 24 hours in milliseconds)
         long tomorrowMillis = System.currentTimeMillis() + (1000 * 60 * 60 * 24);
         String tomorrow = sdf.format(new Date(tomorrowMillis));
 
