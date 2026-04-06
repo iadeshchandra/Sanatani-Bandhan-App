bdbe91f65d1940aa6f564b91bf4829651721f669
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-06 11:49:20 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.1.0' 
-        classpath 'com.google.gms:google-services:4.4.0' 
+        classpath("com.android.tools.build:gradle:8.1.0") 
+        classpath("com.google.gms:google-services:4.4.0") 
         
-        // ✨ NEW: Firebase Crashlytics Classpath
-        classpath 'com.google.firebase:firebase-crashlytics-gradle:2.9.9' 
+        // ✨ FIREBASE CRASHLYTICS (Kotlin Syntax)
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9") 
     }
 }
 
 plugins {
-    id 'com.android.application' version '8.1.0' apply false
+    id("com.android.application") version "8.1.0" apply false
 }
