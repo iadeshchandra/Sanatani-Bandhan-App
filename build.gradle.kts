1e816210154f7499303363d42776322013bafc3e
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-06 11:33:01 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        // Notice the parentheses and double quotes required by Kotlin DSL
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("com.google.gms:google-services:4.4.1")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+        classpath 'com.android.tools.build:gradle:8.1.0' 
+        classpath 'com.google.gms:google-services:4.4.0' 
+        
+        // ✨ NEW: Firebase Crashlytics Classpath
+        classpath 'com.google.firebase:firebase-crashlytics-gradle:2.9.9' 
     }
 }
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
+    id 'com.android.application' version '8.1.0' apply false
 }
