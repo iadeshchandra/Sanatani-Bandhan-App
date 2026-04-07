36779dcfbb748e2d326e478b1cec1a1524de0fcf
Adesh Chandra <105627039+iadeshchandra@users.noreply.github.com>
2026-04-08 00:31:57 +0600

    Update Poll.java

--- app/src/main/java/org/shda/Poll.java
+++ app/src/main/java/org/shda/Poll.java
@@ -3,22 +3,9 @@ package org.shda;
 import java.util.HashMap;
 
 public class Poll {
-    public String id, question, optionA, optionB, optionC, optionD, createdBy, officialComment;
+    public String id, question, optionA, optionB, optionC, optionD, createdBy, adminComment;
     public long timestamp, endTimestamp;
     public HashMap<String, String> votes;
 
-    public Poll() {} // Required for Firebase
-
-    public Poll(String id, String question, String optionA, String optionB, String optionC, String optionD, long timestamp, long endTimestamp, String createdBy) {
-        this.id = id;
-        this.question = question;
-        this.optionA = optionA;
-        this.optionB = optionB;
-        this.optionC = optionC;
-        this.optionD = optionD;
-        this.timestamp = timestamp;
-        this.endTimestamp = endTimestamp;
-        this.createdBy = createdBy;
-        this.votes = new HashMap<>();
-    }
+    public Poll() {}
 }
